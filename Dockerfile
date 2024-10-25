aeb960441db869412c0403cba92ae0c0448e6279
issacamara <isskamara@live.fr>
2024-10-25 22:19:45 +0100

    Adding parent directory to path

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,13 @@
-RUN export PYTHONPATH="${PYTHONPATH}:/Users/issacamara/Developer/tradvisor"
+FROM python:3.9
+WORKDIR tradvisor/
+
+RUN apt-get install -y git
+
+RUN git clone https://github.com/issacamara/tradvisor.git .
+
+RUN pip install -r requirements.txt
+
+#RUN export PYTHONPATH="${PYTHONPATH}:/Users/issacamara/Developer/tradvisor"
+EXPOSE 8501
+
+ENTRYPOINT ["streamlit", "run", "webapp/app.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- webapp/app.py
+++ webapp/app.py
@@ -1,8 +1,11 @@
-import streamlit as st
-import pandas as pd
-from util import trading_indicators as ti
-import yaml
+import sys
+sys.path.append('..')
 import duckdb as db
+import pandas as pd
+import streamlit as st
+import yaml
+import numpy as np
+import util.trading as tr
 
 
 # Load configuration from YAML file
@@ -17,17 +20,26 @@ conn = db.connect(config['duckdb']['database'])
 
 
 # Cache data for 1 day (86400 seconds)
-@st.cache_data(ttl=86400)
+# @st.cache_data(ttl=86400)
 def load_data():
-    query1 = f"SELECT * FROM shares"
+    query1 = f"""SELECT * FROM shares order by date desc"""
+
     query2 = f"SELECT * FROM bonds"
     query3 = f"SELECT * FROM indices"
     query4 = f"SELECT * FROM dividends"
     query5 = f"SELECT * FROM capitalizations"
     shares = conn.execute(query1).df()
-    shares['EMA'] = ti.ema(shares['CLOSING_PRICE'])
     bonds = conn.execute(query2).df()
-    return shares, bonds
+    indices = conn.execute(query3).df()
+    dividends = conn.execute(query4).df()
+    capitalizations = conn.execute(query5).df()
+
+    shares = pd.merge(shares, dividends[["SYMBOL","DIVIDEND","PAYMENT_DATE"]], on='SYMBOL', how='left')
+
+    # shares = tr.get_trading_decisions(shares)
+
+
+    return shares, bonds, indices, dividends, capitalizations
 
 
 # Streamlit app
@@ -38,9 +50,23 @@ st.title("Tradvisor")
 main_container = st.container()
 mt1, mt2 = main_container.tabs(["Top 10 Stocks", "Tab 2"])
 with mt1:
-    shares, bonds = load_data()
-    mt1.write(shares[['SYMBOL','NAME','CLOSING_PRICE','EMA']].head())
-    # mt1.write(bonds.head())
+    shares, bonds, indices, dividends, capitalizations = load_data()
+    # mt1.dataframe(shares.head())
+    for i in range(5):
+        columns = mt1.columns(5)
+        columns[0].write(shares.loc[i, 'NAME'])
+        columns[1].metric('💸💸💸', f"{shares.loc[i, 'CLOSE']:.0f}")
+        columns[2].metric('💸💸💸',shares.loc[i, 'DIVIDEND'])
+        value = shares.loc[i, 'DIVIDEND']
+        if np.isnan(value):
+            value = 0
+        columns[3].metric('ROI',f"{value/shares.loc[i, 'CLOSE']:.1%}" )
+        columns[4].write(shares.loc[i, 'PAYMENT_DATE'])
+
+    # mt1.dataframe(bonds.head())
+    # mt1.dataframe(indices.head())
+    # mt1.dataframe(dividends.head())
+    # mt1.dataframe(capitalizations.head())
 with mt2:
     mt2.write("Coming soon !")
 
