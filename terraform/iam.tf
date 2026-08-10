2a62f0aa8d55cf39bd9f6f48501d046c4b6e609c
Issa Camara <isskamara@live.com>
2026-08-10 18:42:58 +0100

    chore(terraform): refactor IAM member resources to use single member syntax and remove commented code

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -59,21 +59,6 @@ resource "google_secret_manager_secret_version" "gmail_acc_secret_version" {
   secret_data = jsonencode(var.tradvisor_gmail_acc)
 }
 
-# resource "null_resource" "sa_key_file" {
-# #   content  = google_service_account_key.tradvisor_sa_key.private_key
-# #   filename = "../webapp/.streamlit/tradvisor-sa-key.json"
-# #   file_permission = "0600"
-#   triggers = {
-#     private_key = google_service_account_key.tradvisor_sa_key.private_key
-#   }
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       echo "${google_service_account_key.tradvisor_sa_key.private_key}" | base64 --decode > ../webapp/.streamlit/tradvisor-sa-key.json
-#     EOT
-#     interpreter = ["/bin/bash", "-c"]
-#   }
-# }
-
 # Create a service account for the Cloud Run service
 resource "google_service_account" "brvm_dashboard_sa" {
   account_id   = "brvm-dashboard-sa"
@@ -81,170 +66,176 @@ resource "google_service_account" "brvm_dashboard_sa" {
   depends_on   = [google_project_service.apis[7] ]
 }
 
+# =====================================================
+# IAM Members - using google_project_iam_member to avoid override issues
+# =====================================================
 
-resource "google_project_iam_member" "build_sa_roles" {
+# CloudBuild roles
+resource "google_project_iam_member" "build_sa_roles_tradvisor" {
   depends_on = [google_service_account.tradvisor_sa]
   project    = var.project_id
   role       = "roles/cloudbuild.builds.builder"
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}",
-    "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com",
-    "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
-resource "google_project_iam_member" "function_invoker" {
+resource "google_project_iam_member" "build_sa_roles_compute" {
+  depends_on = [google_service_account.tradvisor_sa]
+  project    = var.project_id
+  role       = "roles/cloudbuild.builds.builder"
+  member     = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "build_sa_roles_cloudbuild" {
+  depends_on = [google_service_account.tradvisor_sa]
+  project    = var.project_id
+  role       = "roles/cloudbuild.builds.builder"
+  member     = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+# Cloud Functions invoker
+resource "google_project_iam_member" "function_invoker_tradvisor" {
   project    = var.project_id
   depends_on = [google_service_account.tradvisor_sa]
   role       = "roles/cloudfunctions.invoker"
-  members = [
-      "serviceAccount:${google_service_account.tradvisor_sa.email}",
-    "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
+resource "google_project_iam_member" "function_invoker_compute" {
+  project    = var.project_id
+  depends_on = [google_service_account.tradvisor_sa]
+  role       = "roles/cloudfunctions.invoker"
+  member     = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+# Storage object viewer
 resource "google_project_iam_member" "all_buckets_viewer" {
   project    = var.project_id
   depends_on = [google_service_account.tradvisor_sa]
   role       = "roles/storage.objectViewer"
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}",
-#   "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
-resource "google_project_iam_member" "log_writer" {
+# Logging log writer
+resource "google_project_iam_member" "log_writer_tradvisor" {
   project    = var.project_id
   depends_on = [google_service_account.tradvisor_sa]
   role       = "roles/logging.logWriter"
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}",
-    "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
-# Grant the necessary roles to the service account
+resource "google_project_iam_member" "log_writer_compute" {
+  project    = var.project_id
+  depends_on = [google_service_account.tradvisor_sa]
+  role       = "roles/logging.logWriter"
+  member     = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+# Cloud Run invoker
 resource "google_project_iam_member" "cloud_run_sa_invoker" {
   project    = var.project_id
   role       = "roles/run.invoker"
   depends_on = [google_service_account.tradvisor_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}"
-#     "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
+# Workflows invoker
 resource "google_project_iam_member" "workflow_executor" {
   project    = var.project_id
   role       = "roles/workflows.invoker"
   depends_on = [google_service_account.tradvisor_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}"
-#     "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
-
-resource "google_project_iam_member" "sa_user" {
+# Service Account User
+resource "google_project_iam_member" "sa_user_tradvisor" {
   project    = var.project_id
   role       = "roles/iam.serviceAccountUser"
   depends_on = [google_service_account.tradvisor_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}",
-    "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
+resource "google_project_iam_member" "sa_user_compute" {
+  project    = var.project_id
+  role       = "roles/iam.serviceAccountUser"
+  depends_on = [google_service_account.tradvisor_sa]
+  member     = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+# Secret Manager accessor
 resource "google_project_iam_member" "sms_accessor" {
   project    = var.project_id
   role       = "roles/secretmanager.secretAccessor"
   depends_on = [google_service_account.brvm_dashboard_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}"
-#   "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
 # Grant secret accessor at secret level for SA key
-resource "google_secret_manager_secret_iam_binding" "sa_key_secret_accessor" {
-  project  = var.project_id
-  secret_id = "tradvisor_sa_key"
-  role     = "roles/secretmanager.secretAccessor"
-  members  = ["serviceAccount:${google_service_account.tradvisor_sa.email}"]
+resource "google_secret_manager_secret_iam_member" "sa_key_secret_accessor" {
+  project    = var.project_id
+  secret_id  = "tradvisor_sa_key"
+  role       = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
 # Grant secret accessor at secret level for gmail account
-resource "google_secret_manager_secret_iam_binding" "gmail_secret_accessor" {
-  project  = var.project_id
-  secret_id = "tradvisor_gmail_acc"
-  role     = "roles/secretmanager.secretAccessor"
-  members  = ["serviceAccount:${google_service_account.tradvisor_sa.email}"]
+resource "google_secret_manager_secret_iam_member" "gmail_secret_accessor" {
+  project    = var.project_id
+  secret_id  = "tradvisor_gmail_acc"
+  role       = "roles/secretmanager.secretAccessor"
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
 # Grant secret accessor for OpenRouter API key
-resource "google_secret_manager_secret_iam_binding" "openrouter_api_key_accessor" {
+resource "google_secret_manager_secret_iam_member" "openrouter_api_key_accessor" {
   project    = var.project_id
   secret_id  = "openrouter_api_key"
   role       = "roles/secretmanager.secretAccessor"
-  members    = ["serviceAccount:${google_service_account.tradvisor_sa.email}"]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
   depends_on = [google_secret_manager_secret.openrouter_api_key, google_secret_manager_secret_version.openrouter_api_key_version]
 }
 
-
+# BigQuery roles
 resource "google_project_iam_member" "bq_viewer" {
   project    = var.project_id
   role       = "roles/bigquery.dataViewer"
   depends_on = [google_service_account.tradvisor_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}",
-#     "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
 resource "google_project_iam_member" "bq_data_editor" {
   project    = var.project_id
   role       = "roles/bigquery.dataEditor"
   depends_on = [google_service_account.tradvisor_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}"
-#     "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
 resource "google_project_iam_member" "bq_job_user" {
   project    = var.project_id
   role       = "roles/bigquery.jobUser"
   depends_on = [google_service_account.tradvisor_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}"
-#     "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
+# Cloud Run admin
 resource "google_project_iam_member" "run_admin" {
   project    = var.project_id
   role       = "roles/run.admin"
   depends_on = [google_service_account.tradvisor_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}"
-#     "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
+# Storage admin
 resource "google_project_iam_member" "storage_admin" {
   project    = var.project_id
   role       = "roles/storage.admin"
   depends_on = [google_service_account.tradvisor_sa]
-  members = [
-    "serviceAccount:${google_service_account.tradvisor_sa.email}"
-#     "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-  ]
+  member     = "serviceAccount:${google_service_account.tradvisor_sa.email}"
 }
 
+# Make the Cloud Run service publicly accessible
 resource "google_cloud_run_service_iam_policy" "noauth" {
   location    = google_cloud_run_v2_service.tradvisor_service.location
   project     = google_cloud_run_v2_service.tradvisor_service.project
   service     = google_cloud_run_v2_service.tradvisor_service.name
-
   policy_data = data.google_iam_policy.noauth.policy_data
 }
