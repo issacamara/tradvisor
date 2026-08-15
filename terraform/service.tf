e2f0389b36834c476e1077e6c080356f6ff04fc1
Issa Camara <isskamara@live.com>
2026-08-15 19:18:51 +0100

    fix(service): comment out unused docker_image variable in Cloud Run service configuration

--- terraform/service.tf
+++ terraform/service.tf
@@ -29,7 +29,7 @@ resource "google_cloud_run_v2_service" "tradvisor_service" {
     }
 
     containers {
-      image = var.docker_image
+      #image = var.docker_image
       ports { container_port = 8501 }
       image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.tradvisor.repository_id}/${var.service_name}:latest"
       env {
